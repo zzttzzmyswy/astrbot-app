d60a261faefb1f14edb873c8f7863b30d5e2e388
zzttzzmyswy <zzttzzmyswy@users.noreply.github.com>
2026-06-19 13:38:06 +0800

    chore: bump version 1.1.0 -> 1.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "top.zztweb.astrbot"
         minSdk = flutter.minSdkVersion
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
